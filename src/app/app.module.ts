95cebfba91c4147f8b44f62bec0e63e22b5cfd22
shahil <shahilcode@gmail.com>
2023-03-03 14:49:09 +0530

    added the commenting form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { SinglePostComponent } from './pages/single-post/single-post.component';
 import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-and-condition.component';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
+import { CommentFormComponent } from './comments/comment-form/comment-form.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { SubscriptionFormComponent } from './subscription-form/subscription-form
     SinglePostComponent,
     TermsAndConditionComponent,
     ContactUsComponent,
-    SubscriptionFormComponent
+    SubscriptionFormComponent,
+    CommentFormComponent
   ],
   imports: [
     BrowserModule,
